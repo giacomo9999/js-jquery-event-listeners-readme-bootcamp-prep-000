e8cd3e6ca44cf2bb3d2cdc7457341113354f878f
James Gary <bucket@earthlink.net>
2018-01-04 18:06:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,24 @@
-//define functions here
+function getIt() {
+  $("p").on("click", function() {
+      alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+      alert("Your form is going to be submitted now.")
+  });
+}
+
+function pressIt() {
+  
+}
 
 $(document).ready(function(){
 
